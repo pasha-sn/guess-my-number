a74ab821d2a88f73b3e7ddbbb53900ab5f48f966
Pasha Sadi <pasha.sn@gmail.com>
2021-08-21 16:12:30 -0400

    Second Commit

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
 'use strict';
-console.log(document.querySelector('.message'));
-// document.querySelector('.message').textContent = 'pasha';
+/*
+//72. Selecting and Elements Manipulation
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number🎉';
+console.log(document.querySelector('.number'));
+console.log(document.querySelector('.number').textContent);
+document.querySelector('.number').textContent = '!';
+console.log(document.querySelector('.number').textContent);
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 27;
+console.log(document.querySelector('.guess').value);
+
+
+
+
+*/
+
+// 73. Handeling Click Events
+
+// document.querySelector('.check').addEventListener('click', function () {
+//   console.log(document.querySelector('.guess').value);
+// });
+
+// document.querySelector('.check').addEventListener('click', function () {
+//   console.log(document.querySelector('.guess').value);
+//   document.querySelector('.message').textContent = 'Correct Number🎉';
+// });
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  }
+});
